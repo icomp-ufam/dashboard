b98f2d6c0d0bee50952b76ac6b132942321ed6a1
Marcos Soares Souza <mss3@icomp.ufam.edu.br>
2016-11-29 21:24:58 -0400

    listagem de vendedores a partir de serviço node local e correções no template

---
+++ vendedoresController.js
@@ -0,0 +1,42 @@
+/**
+ * Created by marcos on 29/11/16.
+ */
+angular.module("vendedores", ["ngMessages"]);
+angular.module("vendedores").controller("vendedoresCtrl", function ($scope, $http) {
+
+    $scope.app = "Vendedores";
+    $scope.vendedores = [];
+
+    var carregarVendedores = function () {
+        $http.get("http://localhost:3412/vendedores").success(function (data) {
+            $scope.vendedores = data;
+        }).error(function (data, status) {
+            $scope.message = "Aconteceu um problema: " + data;
+        });
+    };
+
+    $scope.adicionarVendedor = function (vendedor) {
+        vendedor.data = new Date();
+        $http.post("http://localhost:3412/vendedores", vendedor).success(function (data) {
+            delete $scope.vendedor;
+            $scope.vendedorForm.$setPristine();
+            carregarvendedores();
+        });
+    };
+    $scope.apagarVendedores = function (vendedores) {
+        $scope.vendedores = vendedores.filter(function (vendedor) {
+            if (!vendedor.selecionado) return vendedor;
+        });
+    };
+    $scope.isVendedoreSelecionado = function (vendedores) {
+        return vendedores.some(function (vendedor) {
+            return vendedor.selecionado;
+        });
+    };
+    $scope.ordenarPor = function (campo) {
+        $scope.criterioDeOrdenacao = campo;
+        $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+    };
+
+    carregarVendedores();
+});
