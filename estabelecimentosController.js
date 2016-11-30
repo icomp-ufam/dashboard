571db5a61d182824ed0bbc6f67c68bd36915399d
LARISSAFABIOLA\Larissa <larissafabiola051@gmail.com>
2016-11-30 16:05:39 -0400

    Adicionando listagem e controller de clientes e estabelecimentos. Adicionar href em algumas views

---
+++ estabelecimentosController.js
@@ -0,0 +1,46 @@
+/**
+ * Created by Larissa Fabíola on 30/11/16.
+ */
+angular.module("estabelecimentos", ["ngMessages"]);
+angular.module("estabelecimentos").controller("estabelecimentosCtrl", function ($scope, $http) {
+
+    $scope.app = "Estabelecimentos";
+    $scope.estabelecimentos = [
+                {nome: "Vendedor1", telefone: "123", nVendedores: "50"},
+                {nome: "Vendedor2", telefone: "456", nVendedores: "20"},
+                {nome: "Vendedor3", telefone: "789", nVendedores: "34"}
+    ];
+
+    var carregarEstabelecimentos = function () {
+        $http.get("http://localhost:3412/estabelecimentos").success(function (data) {
+            $scope.estabelecimentos = data;
+        }).error(function (data, status) {
+            $scope.message = "Aconteceu um problema: " + data;
+        });
+    };
+
+    $scope.adicionarEstabelecimento = function (estabelecimento) {
+        estabelecimento.data = new Date();
+        $http.post("http://localhost:3412/estabelecimentos", estabelecimento).success(function (data) {
+            delete $scope.estabelecimento;
+            $scope.estabelecimentoForm.$setPristine();
+            carregarestabelecimentos();
+        });
+    };
+    $scope.apagarestabelecimentos = function (estabelecimentos) {
+        $scope.estabelecimentos = estabelecimentos.filter(function (estabelecimento) {
+            if (!estabelecimento.selecionado) return estabelecimento;
+        });
+    };
+    $scope.isEstabelecimentoselecionado = function (estabelecimentos) {
+        return estabelecimentos.some(function (estabelecimento) {
+            return estabelecimento.selecionado;
+        });
+    };
+    $scope.ordenarPor = function (campo) {
+        $scope.criterioDeOrdenacao = campo;
+        $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+    };
+
+    carregarEstabelecimentos();
+});
