3ad661ed8f0027f7692c6db868927a367beb7c1f
Marcos Soares Souza <mss3@icomp.ufam.edu.br>
2016-12-01 13:04:53 -0400

    adicionando controllers e removendo barra de pesquisa

---
+++ form_adminController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by marcos on 01/12/16.
+ */
+angular.module("form_admin", ["ngMessages"]);
+angular.module("form_admin").controller("form_adminCtrl", function ($scope, $http) {
+
+});
---
+++ indexController.js
@@ -0,0 +1,7 @@
+/**
+ * Created by marcos on 01/12/16.
+ */
+angular.module("index", ["ngMessages"]);
+angular.module("index").controller("indexCtrl", function ($scope, $http) {
+
+});
