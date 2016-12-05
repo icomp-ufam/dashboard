ae4e7cc1d3b51290d64baf654238d332a86d8f6f
Marcos Soares Souza <mss3@icomp.ufam.edu.br>
2016-12-05 17:12:43 -0400

    estrutura organizada em rotas

--- form_adminController.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by marcos on 01/12/16.
- */
-angular.module("form_admin", ["ngMessages"]);
-angular.module("form_admin").controller("form_adminCtrl", function ($scope, $http) {
-
-});
--- indexController.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by marcos on 01/12/16.
- */
-angular.module("index", ["ngMessages"]);
-angular.module("index").controller("indexCtrl", function ($scope, $http) {
-
-});
