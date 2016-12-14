33593f07c2b6f153404a7858e11bfc3e744ab4f0
Larissa Bentes <lgpbentes@gmail.com>
2016-12-13 23:53:27 -0400

    Adicionado gráfico de média de consultas por hora

--- app/assets/js/demo_charts_morris.js
+++ app/assets/js/demo_charts_morris.js
@@ -1,25 +1,26 @@
 var xmlhttp = new XMLHttpRequest();
 var url = "http://54.233.67.111:8081/analytics/hourly/queries/day/avg";
 var myArr;
-console.log(1);
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
 xmlhttp.onload = function() {
     if (this.readyState == 4 && this.status == 200) {
         myArr = JSON.parse(this.responseText);
-
     }
+
+    //ordena pela hora (label)
+    myArr.sort(function(a,b) {return a.hora - b.hora});
+    dataJ = [];
+
+    //cria dados para o grafico baseado no json de resposta
+    for (el in myArr)
+        dataJ.push({label: myArr[el].hora+":hrs", value: myArr[el].media_casos});
 
     Morris.Donut({
         element: 'morris-donut-example',
-        data: [
-
-            {label: "teste1", value: myArr[0].media_casos},
-            {label: "teste2", value: myArr[1].media_casos},
-            {label: "teste3", value: myArr[2].media_casos}
-        ],
-        colors: ['#95B75D', '#1caf9a', '#FEA223']
+        data: dataJ,
+        colors: ['#95B75D', '#1caf9a', '#FEA223', "#33414e", "#D32F2F"]
     });
 };
 
