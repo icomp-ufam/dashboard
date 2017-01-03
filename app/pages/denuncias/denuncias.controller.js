b6e24351e40bbd02104e3c39df03ee142cf4b00e
Icaro Feitosa Dolzane <ikro.dolzane@gmail.com>
2017-01-03 16:39:58 -0400

    Listar denúncias FINALIZADO.

--- app/pages/denuncias/denuncias.controller.js
+++ app/pages/denuncias/denuncias.controller.js
@@ -13,8 +13,8 @@ angular.module("teewa").controller("denunciasCtrl", function ($scope, $http) {
                 'Authorization' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0ODA2MjA2MjZ9.LL1jFE5Epo22h2usXTIEKySbUTGtSZlBpfWsQEL8nOk'
             },
             data: {
-                'date_start' : 01/01/2016,
-                'date_end' : 22/12/2016
+                'date_start' : '01/01/2015',
+                'date_end' : '24/12/2016'
             }
         }).success(function(data){
             $scope.denuncias = data;
