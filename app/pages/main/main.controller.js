d2238455510e11968f8aa7e29471d0352ac9a807
LGugs <lgpa@icomp.ufam.edu.br>
2017-01-11 19:02:35 -0400

    Ajustado status de usuario ao fazer login e logout

--- app/pages/main/main.controller.js
+++ app/pages/main/main.controller.js
@@ -1,16 +1,30 @@
 /**
- * Created by Marcos Soares on 10/01/17.
+ * Created by Marcos Soares on 10/01/17
+ * Edited by Luiz Gustavo on 11/01/17
  */
-angular.module("teewa").controller("mainCtrl", function ($scope, $state, config) {
+angular.module("teewa").controller("mainCtrl", function ($scope, $state, config, sharedConn, ChatDetails) {
 	$scope.state = $state;
-	$scope.vendedor = false;
+	$scope.vendedor = false; // verificar!!!, esta voltando 
     //stub de login
-	$scope.login = function () {
-		$scope.vendedor = true;
-    }
-    $scope.logout = function () {
+	$scope.login = function(user) {
+        sharedConn.login(config.user,'myserver',config.password);
+        $scope.chats = sharedConn.getRoster();
+        $scope.hideTime = true;
+        $scope.data = {};
+        $scope.myId = sharedConn.getConnectObj().jid;
+        $scope.messages = [];
+        $scope.to_id = ChatDetails.getTo();
+        $scope.vendedor = true;
+    };
+	$scope.logout = function() {
+        console.log("T");
+        sharedConn.logout();
+        $state.go('login', {}, {
+            location: "replace",
+            reload: true
+        });
         $scope.vendedor = false;
-    }
+    };
     $scope.verifica = function () {
 		return $scope.vendedor;
     }
