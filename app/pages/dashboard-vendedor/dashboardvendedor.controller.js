c8ebdd6c3b819155097996159ecfd8dc1b09c58d
Larissa Bentes <lgpbentes@gmail.com>
2017-01-10 21:57:23 -0400

    Fazendo aceitar/recusar casos; Chat com informações do caso.

--- app/pages/dashboard-vendedor/dashboardvendedor.controller.js
+++ app/pages/dashboard-vendedor/dashboardvendedor.controller.js
@@ -1,14 +1,17 @@
 /**
  * Created by lgpbentes on 09/01/17.
  */
-angular.module("teewa").controller("dashboardVendedorCtrl", function ($scope, $http, config) {
+angular.module("teewa").controller("dashboardVendedorCtrl", function ($scope, $http, config, $state) {
     $scope.app = "Dashboard Vendedor";
     $scope.chats = [];
     $scope.casos = [];
     $scope.qteChats = 0;
     $scope.qteCasos = 0;
+    $scope.chatAtual = "";
 
     $scope.urlPhotos = config.baseUrl + "/photos/";
+    $scope.urlFiles = config.baseUrl + "/case_images/";
+
     // id do erick
     $scope.idVendedor = '650';
 
@@ -39,7 +42,9 @@ angular.module("teewa").controller("dashboardVendedorCtrl", function ($scope, $h
                 'Authorization' : 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE0ODA2MjA2MjZ9.LL1jFE5Epo22h2usXTIEKySbUTGtSZlBpfWsQEL8nOk'
             },
             data: {
+                //id do teewa
                 'idstore' : '1',
+                //id do caio
                 'idseller' :'652'
             }
         }).success(function(data){
@@ -49,6 +54,26 @@ angular.module("teewa").controller("dashboardVendedorCtrl", function ($scope, $h
         }).error(function(error){
             $scope.message = "Aconteceu um problema: " + data;
         });
+    };
+
+    $scope.aceitarCaso = function (idcase) {
+        // so fazer essa parada quando tiver contas de teste
+        // rota: /cases/accept, metodo PUT, params: idseller, idcase, idstore
+        console.log(idcase);
+        //criar a sala, passar o id do chat para a tela de chat (?)
+        $state.go("main.dashboardVendedor.casosAbertos");
+    };
+
+    $scope.recusarCaso = function (idcase) {
+        // so fazer essa parada quando tiver contas de teste
+        // rota: /cases/deny, metodo PUT, params: idseller, idcase, idstore
+        console.log(idcase);
+        //apenas recarregar a pagina
+        $state.reload();
+    };
+
+    $scope.clickChat = function (nome) {
+        $scope.chatAtual = nome;
     };
 
     $scope.login = function () {
