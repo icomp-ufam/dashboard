1e6c650fd3017e9d17bee573265ad957f9f1298e
duivilly.brito <duivilly.brito@tce.am.gov.br>
2017-02-22 11:51:59 -0400

    modif panel do dashboard admin

--- app/pages/dashboard/dashboard.controller.js
+++ app/pages/dashboard/dashboard.controller.js
@@ -13,8 +13,11 @@ angular.module("teewa").controller("dashboardCtrl", function ($scope, $state, $h
     $scope.cases = [];
     $scope.clientes = [];
     $scope.atendimentos = [];
+    $scope.atendimentosAntes = [];
     $scope.estabelecimentos = [];
+    $scope.estabelecimentosAntes = [];
     $scope.denuncias = [];
+    $scope.denunciasAntes = [];
     //$scope.teste = $rootScope.usuario;
 
     var carregarClientes = function () {
@@ -28,15 +31,37 @@ angular.module("teewa").controller("dashboardCtrl", function ($scope, $state, $h
         }).success(function(data){
             $scope.clientes = data;
             $scope.qtClientes = $scope.clientes.users.length;
+            $scope.qtClientesAntes = 0;
             //$scope.qtClientes =
         }).error(function(error){
             $scope.message = "Aconteceu um problema: " + data;
             console.log("login error");
         });
     };
+
+
     $scope.carregarAtendimentos = function () {
         //console.log(date_start);
         //console.log(date_end);
+        var today= new Date();
+        var ddToday= today.getDate();
+        var mmToday= today.getMonth()+1;
+        var yyToday= today.getFullYear();
+        var dataToday= ddToday+'/'+mmToday+'/'+yyToday;
+
+
+        //quantidade de dias para voltar
+        var dParaVoltar= 7;
+        ///////////////////
+        var ddAntes= ddToday;
+        var mmAntes= mmToday;
+        var yyAntes= yyToday;
+        var todayS= mmAntes+'/'+ddAntes+'/'+yyAntes;
+
+        var myDate = new Date(todayS);
+        var dayOfMonth = myDate.getDate();
+        myDate.setDate(dayOfMonth - dParaVoltar);
+        var todayAntes= myDate.getDate()+'/'+(myDate.getMonth()+1)+'/'+myDate.getFullYear();
 
         $http({
 
@@ -48,7 +73,7 @@ angular.module("teewa").controller("dashboardCtrl", function ($scope, $state, $h
             },
             data: {
                 'date_start' : '01/01/2016',
-                'date_end' : '31/12/2016',
+                'date_end' : dataToday,
                 //'idcategory' : '5'
             }
         }).success(function(data,date){
@@ -59,9 +84,42 @@ angular.module("teewa").controller("dashboardCtrl", function ($scope, $state, $h
             $scope.message = "Aconteceu um problema: " + error;
             console.log("login error");
         });
+
+
+        $http({
+
+            url : config.baseUrl + "/dash/calls/",
+            method : 'post',
+            headers : {
+                'Content-Type': 'application/json',
+                'Authorization' : config.token
+            },
+            data: {
+                'date_start' : todayAntes,
+                'date_end' : dataToday,
+                //'idcategory' : '5'
+            }
+        }).success(function(data,date){
+            $scope.atendimentosAntes = data;
+            //calculo em percentagem
+            var t3= $scope.qtAtendimentos;
+            var n3= $scope.atendimentosAntes.length;
+            var p3= ((t3-(t3-n3))/t3)*100;
+            if(p3 < 0){
+                p3= p3*-1;
+            }
+            $scope.qtAtendimentosAntes= $scope.atendimentosAntes.length;
+            $scope.qtAtendimentosAntesPer= p3;
+
+            //console.log($scope.qtAtendimentos)
+        }).error(function(error){
+            $scope.message = "Aconteceu um problema: " + error;
+            console.log("login error");
+        });
             
 
     };
+
 
     var carregarCases = function () {
         $http({
@@ -81,6 +139,7 @@ angular.module("teewa").controller("dashboardCtrl", function ($scope, $state, $h
 
     };
 
+
     $scope.UserbyCases = [];
     var carregarUsuariosEcasos = function () {
         $http({
@@ -94,7 +153,29 @@ angular.module("teewa").controller("dashboardCtrl", function ($scope, $state, $h
         });
     };
 
+
+
     $scope.carregarEstabelecimentos = function () {
+        var today= new Date();
+        var ddToday= today.getDate();
+        var mmToday= today.getMonth()+1;
+        var yyToday= today.getFullYear();
+        var dataToday= ddToday+'/'+mmToday+'/'+yyToday;
+
+
+        //quantidade de dias para voltar
+        var dParaVoltar= 7;
+        ///////////////////
+        var ddAntes= ddToday;
+        var mmAntes= mmToday;
+        var yyAntes= yyToday;
+        var todayS= mmAntes+'/'+ddAntes+'/'+yyAntes;
+
+        var myDate = new Date(todayS);
+        var dayOfMonth = myDate.getDate();
+        myDate.setDate(dayOfMonth - dParaVoltar);
+        var todayAntes= myDate.getDate()+'/'+(myDate.getMonth()+1)+'/'+myDate.getFullYear();
+
         $http({
 
             url : config.baseUrl + "/dash/store",
@@ -105,7 +186,7 @@ angular.module("teewa").controller("dashboardCtrl", function ($scope, $state, $h
             },
             data: {
                 'date_start' : '01/01/2016',
-                'date_end' : '31/12/2016'
+                'date_end' : dataToday
             }
         }).success(function(data){
             $scope.estabelecimentos = data;
@@ -116,11 +197,67 @@ angular.module("teewa").controller("dashboardCtrl", function ($scope, $state, $h
         }).error(function(error){
             $scope.message = "Aconteceu um problema: " + error;
         });
-    };
 
-    $scope.carregarDenuncias = function () {
+
         $http({
 
+            url : config.baseUrl + "/dash/store",
+            method : 'post',
+            headers : {
+                'Content-Type': 'application/json',
+                'Authorization' : config.token
+            },
+            data: {
+                'date_start' : todayAntes,
+                'date_end' : dataToday
+            }
+        }).success(function(data){
+            $scope.estabelecimentosAntes = data;
+            //calculo em percentagem
+            var t2= $scope.qtEstalecimentos;
+            var n2= $scope.estabelecimentosAntes.length;
+            var p2= ((t2-(t2-n2))/t2)*100;
+            if(p2 < 0){
+                p2= p2*-1;
+            }
+            $scope.qtEstalecimentosAntes= $scope.estabelecimentosAntes.length;
+            $scope.qtEstalecimentosAntesPer= p2;
+
+            //console.log("estabelecimentos: " + $scope.qtEstalecimentos );
+
+        }).error(function(error){
+            $scope.message = "Aconteceu um problema: " + error;
+        });
+    };
+
+
+
+    $scope.carregarDenuncias = function () {
+        var today= new Date();
+        var ddToday= today.getDate();
+        var mmToday= today.getMonth()+1;
+        var yyToday= today.getFullYear();
+        var dataToday= ddToday+'/'+mmToday+'/'+yyToday;
+
+
+        //quantidade de dias para voltar
+        var dParaVoltar= 7;
+        ///////////////////
+        var ddAntes= ddToday;
+        var mmAntes= mmToday;
+        var yyAntes= yyToday;
+        var todayS= mmAntes+'/'+ddAntes+'/'+yyAntes;
+
+        var myDate = new Date(todayS);
+        var dayOfMonth = myDate.getDate();
+        myDate.setDate(dayOfMonth - dParaVoltar);
+        var todayAntes= myDate.getDate()+'/'+(myDate.getMonth()+1)+'/'+myDate.getFullYear();
+
+        //document.write('hoje: '+dataToday+' --- ');
+        //document.write('anterior: '+todayAntes);
+
+//pega todas as denuncias e o total
+        $http({
             url : config.baseUrl + "/dash/complaints",
             method : 'post',
             headers : {
@@ -128,8 +265,8 @@ angular.module("teewa").controller("dashboardCtrl", function ($scope, $state, $h
                 'Authorization' : config.token
             },
             data: {
-                'date_start' : '01/01/2016',
-                'date_end' : '31/12/2020'
+                'date_start' : '1/1/2016',
+                'date_end' : dataToday
             }
         }).success(function(data){
             $scope.denuncias = data;
@@ -140,6 +277,38 @@ angular.module("teewa").controller("dashboardCtrl", function ($scope, $state, $h
         }).error(function(error){
             $scope.message = "Aconteceu um problema: " + error;
         });
+
+//pega as denuncias de um dia ou uma semana e o total
+        $http({
+            url : config.baseUrl + "/dash/complaints",
+            method : 'post',
+            headers : {
+                'Content-Type': 'application/json',
+                'Authorization' : config.token
+            },
+            data: {
+                'date_start' : todayAntes,
+                'date_end' : dataToday
+            }
+        }).success(function(data){
+            $scope.denunciasAntes = data;
+            //calculo em percentagem
+            var t1= $scope.qtDenuncias;
+            var n1= $scope.denunciasAntes.length;
+            var p1= ((t1-(t1-n1))/t1)*100;
+            if(p1 < 0){
+                p1= p1*-1;
+            }
+            $scope.qtDenunciasAntes= $scope.denunciasAntes.length;
+            $scope.qtDenunciasAntesPer= p1;
+
+            //document.write(p);
+           
+            //console.log("denuncias: " + $scope.qtDenuncias);
+
+        }).error(function(error){
+            $scope.message = "Aconteceu um problema: " + error;
+        });        
     };
 
 
@@ -149,4 +318,4 @@ angular.module("teewa").controller("dashboardCtrl", function ($scope, $state, $h
     $scope.carregarAtendimentos();
     $scope.carregarEstabelecimentos();
     $scope.carregarDenuncias();
-});
+}); 
